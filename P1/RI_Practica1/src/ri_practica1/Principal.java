2190754fd4b2a16e8ae8f9434b661400b63948c4
alonso-bh <alonsobueno13@correo.ugr.es>
2020-09-29 12:58:58 +0200

    subido p1

---
+++ P1/RI_Practica1/src/ri_practica1/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ri_practica1;
+
+import java.io.File; 
+import org.apache.tika.*; 
+
+
+/**
+ *
+ * @author alonso
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
